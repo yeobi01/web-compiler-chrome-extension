1583c5d4a4d61c6343b932238959f1b61c8638da
yeobi01 <yeobi_01@naver.com>
2025-03-25 15:17:30 +0900

    feat: content_script 에서 보내는 메시지 받는 함수 추가

--- src/service_worker.ts
+++ src/service_worker.ts
@@ -1,3 +1,11 @@
+// import storage from "./utils/localstorage";
+
 chrome.sidePanel
   .setPanelBehavior({ openPanelOnActionClick: true })
   .catch((error) => console.error(error));
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === "BAEKJOON_PROBLEM_DATA") {
+    sendResponse({ sender, status: "200" });
+  }
+});
