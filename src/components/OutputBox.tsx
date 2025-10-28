f1c4a327af1148c231297f7cf47cf0e9853d321c
o-bard-o <i4song.dev@gmail.com>
2025-10-29 00:28:04 +0900

    [FEAT/11] 컴파일 에러 화면상에 출력

--- src/components/OutputBox.tsx
+++ src/components/OutputBox.tsx
@@ -5,17 +5,31 @@ interface OutputBoxProps {
 }
 
 export default function OutputBox({ outputData }: OutputBoxProps) {
+
+  const sanitizeOutput = (output: string) => {
+    const filename = output.split(":")[0];
+    console.log(output);
+    return output.replaceAll(filename, "main.cpp");
+  }
+
   return (
     <div className="h-1/4 flex flex-col px-4 items-start">
       <div className="flex flex-row w-full justify-between items-center mb-2">
         <div className="font-bold text-[1rem] text-gray-300">실행 결과</div>
       </div>
       <div className="flex flex-col h-full w-full overflow-y-scroll output-scroll pr-2">
-        {outputData ? null : (
+        {!outputData && (
           <p className="self-start text-gray-300">
             실행 결과가 여기에 표시됩니다.
           </p>
         )}
+        {
+          outputData?.compileError && (
+            <p className="self-start text-red-400 text-start font-mono">{sanitizeOutput(outputData.compileOutput).split("\n").map(line => {
+              return <pre key={line}>{"ㅤ" + line}<br/></pre>;
+            })}</p>
+          )
+        }
         {outputData?.outputs?.map((data, index) => (
           <div className="w-full flex flex-col items-start p-2 text-[0.9rem] output-border whitespace-pre-line">
             <p className="font-bold text-gray-300">테스트 {index}</p>
